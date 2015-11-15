4863869d300f4914c63836c512ff9738a8305d7b
Eijah Duane Prince <Prince@Eijahs-MacBook-Pro.local>
2015-11-14 22:49:06 -0600

    done

--- excercises-2.js
+++ excercises-2.js
@@ -4,13 +4,14 @@
 
 function max(num1, num2){
     if (num1 > num2) {
-		return true;
+		return num1;
 	} else {
-		return false;
+		return num2;
 	}
 }
 
   console.log (max(0, 150));
+
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
@@ -31,7 +32,7 @@ function maxOfThree(num1, num2, num3){
 	else {
 		return num3;
 	}
-}
+
    }
    var arr = [1,150,350]
    var max = Math.max (arr)
@@ -41,47 +42,119 @@ function maxOfThree(num1, num2, num3){
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // ---------------------
 
-function isVowel(char){
-   if (char) {}
-}
+var findVowel = function(letter) {
+
+    var vowels = ["a", "e", "i", "o", "u"];
+
+    for(var i = 0; i < vowels.length; i++){
+        if(letter === vowels[i]){
+            return true;
+         }
+    }
+    
+    return false;
+
+};
+
 
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
-function rovarspraket(phrase){
-    //...
+function checkConsonants(letterToCheck) {
+    var consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+    var isConsonant = false;
+
+    for (i = 0; i < consonants.length; i++) {
+        if (letterToCheck == consonants[i]) {
+            isConsonant = true;
+        }
+    }
+    return isConsonant;
 }
+
+function translate(funString, letterO) {
+
+
+    var newString = '';
+
+    for (var i = 0; i < funString.length; i++) {
+        if (checkConsonants(funString[i])) {
+            newString += funString[i] + letterO + funString[i];
+        } else {
+            newString += funString[i];
+        }
+    }
+    console.log( '"' + newString + '"');
+}
+
+translate('this is fun', 'o');
 
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse(){
-    //...
+function reverse(str) {
+    var text = '';
+    
+    for (var i = str.length - 1; i >= 0; i--) {
+    	text += str[i];
+    	continue
+    }
+    return text.toLowerCase();
 }
+console.log(reverse("Kosal"));
 
 // ---------------------
 // Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
 // ---------------------
 
-function findLongestWord(words){
-    //...
+function longestWord(string) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length - 1; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
 }
-
 // ---------------------
 // Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
 // ---------------------
 
-function filterLongWords(words, i){
-    //...
-}
+function filterWords(words, i){
+		var filtered = new Array();
+		var j = 0;
+		for(var m = 0; m < words.length; m++){
+			if(words[m].length > i){
+				filtered[j] = words[m];
+				j++;
+			}
+		}
+		return filtered;
+	}
+
+
 
 // ---------------------
 // Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
 // ---------------------
 
-function charFreq(string){
-    //...
+
+var charFreq = function(string){
+    var array = string.split("");
+    var frequency = {};
+    console.log(array.indexOf("a"));
+   
+    array.forEach(function(element){
+        if (!frequency[element]) {frequency[element] = 0};
+        frequency[element] += 1;  
+    })
+    return frequency
 }
+
+console.log(charFreq("abbabcbdbabdbdbabababcbcbab"));
--- test.js
+++ test.js
@@ -1,20 +1,130 @@
-function maxOfThree(num1, num2, num3){
-	if (num1> num2) {
-		if (num1 > num3) {
-			return num1;
-		}
-		else {
-			return num3;
-		}
-	}
-	else if (num2 > num3) {
-		return num2;
-	}
-	else {
-		return num3;
+// ---------------------
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// ---------------------
+
+function max(num1, num2){
+    if (num1 > num2) {
+		return true;
+	} else {
+		return false;
 	}
 }
+
+  console.log (max(0, 150));
+
+// ---------------------
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// ---------------------
+
+function maxOfThree(num1, num2, num3){
+return math.max(a , b , c );
+
+// ---------------------
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// ---------------------
+
+function isVowel(char){
+   var vowels = ["a", "e" , "i" , "o" ,"u"];
+   var isv = false;
+
+   vowels.forEach(function(value){
+   if (char === value){
+   	isv = true
+
    }
-   var arr = [1,150,350]
-   var max = Math.max(arr)
-  console.log (maxOfThree(arr);
+});
+   return isv; 
+}
+
+// ---------------------
+// Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
+// ---------------------
+
+function rovarspraket(phrase){
+    //...
+    var newstr = ''
+    for (var i = 0; i < phrase.length; i += 1){
+    	var currentCar = phrase.charAt(i);
+    	if (isVowel)(currentChar)) {
+	newStr += current;
+} else{newstr += currentChar + 'o' + currentChar
+}
+return  newstr
+}
+rovarspraket("this is fun")
+    }
+}
+
+
+// ---------------------
+// Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
+// ---------------------
+
+function reverse(str){
+	return str.split('').reverse().join('')
+    //...
+    return words.reduce(function(a , b ) {
+    	if (a.length > b.length) {
+    		return a;
+
+    	}
+    	return b
+    	// body...
+    });
+
+}
+
+// ---------------------
+// Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
+// ---------------------
+
+function findLongestWord(words){
+    //...
+       return words.filter(function(value)){
+if (value.length > i){
+	return value
+    });
+}
+}
+
+// ---------------------
+// Write a function filterLongWords() that takes an array of words and an integer i and returns the array of words that are longer than i.
+// ---------------------
+
+function filterLongWords(words, i){
+
+    //...
+    return words.filter(function(value)){
+if (value.length > i){
+	return value
+    });
+}
+}
+filterLongWords(["important", "is", "supercalifrigilistec", "a"] ,3)
+// ---------------------
+// Write a function charFreq() that takes a string and builds a frequency listing of the characters contained in it. Represent the frequency listing as a Javascript object. Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").
+// ---------------------
+
+function charFreq(string){
+    //...function letterCount(str) {
+  if (str) {
+    var letterObj = {};
+    var currentLetter = "";
+    var regEx = /^[A-Za-z0-9]*$/;
+    for (var i = 0; i < str.length; i += 1) {
+      currentLetter = str.charAt(i);
+      if (regEx.test(currentLetter)) {
+        if (letterObj[currentLetter] !== undefined) {
+          letterObj[currentLetter] += 1;
+        } else {
+          letterObj[currentLetter] = 1;
+        }
+      }
+    }
+    return letterObj;
+  } else {
+    return {};
+  }
+}
+
+}
